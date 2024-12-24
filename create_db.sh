fb7b2c6d24875d1f94eb900568ea67a4bc18c003
moeendehqan <moeen.dehqan@gmail.com>
2024-12-24 21:50:39 +0330

    Enhance admin user creation feedback in create_db.sh
    - Added a success message in Persian to confirm the creation of the admin user during the database setup process, improving user experience and clarity.

--- create_db.sh
+++ create_db.sh
@@ -37,5 +37,6 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 if not User.objects.filter(username='admin').exists():
     User.objects.create_superuser('admin', 'admin@example.com', 'RoundShop2024!')
+print('کاربر ادمین با موفقیت ایجاد شد.')
 "
 
